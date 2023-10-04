0bbc94e7afa0ac0f1e7ae521cf187f8e6f15e1d3
Pochieh Wang <pochieh0727@gmail.com>
2023-10-04 13:33:53 -0500

    Fixed bug and provided validation

--- MP2/MultithreadingMatrixMultiplication.java
+++ MP2/MultithreadingMatrixMultiplication.java
@@ -18,11 +18,9 @@ public class MultithreadingMatrixMultiplication extends Thread {
         System.out.println("Now running thread" + this.threadNum);
         for (int row = 0; row < 4; row++) {
             for (int col = 0; col < 20; col++) {
-                int temp = 0;
                 for (int i = 0; i < 20; i++) {
-                    temp += arr1[row][i] * arr2[col][i];
+                    this.result[this.threadNum * 4 + row][col] += arr1[this.threadNum * 4 + row][i] * arr2[i][col];
                 }
-                this.result[this.threadNum * 4 + row][col] = temp;
             }
         }
     }
@@ -32,22 +30,26 @@ public class MultithreadingMatrixMultiplication extends Thread {
     }
 
     public static void main(String[] args) {
-        // Create arr 1
+        // Create input array 1
         Random rand = new Random();
         int[][] arr1 = new int[20][20];
         for (int i = 0; i < 20; i++) {
             for (int j = 0; j < 20; j++) {
-                arr1[i][j] =  rand.nextInt(10);
+                arr1[i][j] = rand.nextInt(10);
             }
         }
+        System.out.println("Input array 1:");
+        System.out.println(Arrays.deepToString(arr1));
 
-        // Create arr 2
+        // Create input array 2
         int[][] arr2 = new int[20][20];
         for (int i = 0; i < 20; i++) {
             for (int j = 0; j < 20; j++) {
                 arr2[i][j] = rand.nextInt(10);
             }
         }
+        System.out.println("Input array 2:");
+        System.out.println(Arrays.deepToString(arr2));
 
         // Create result and thread array
         int[][] ans = new int[20][20];
@@ -69,6 +71,25 @@ public class MultithreadingMatrixMultiplication extends Thread {
         }
 
         // Print out result
+        System.out.println("Result of multithreading:");
         System.out.println(Arrays.deepToString(ans));
+
+        // Calculate the result by single thread to validate the result of
+        // multithreading
+        int[][] singleThreadResult = new int[20][20];
+        for (int row = 0; row < 20; row++) {
+            for (int col = 0; col < 20; col++) {
+                for (int i = 0; i < 20; i++) {
+                    singleThreadResult[row][col] += arr1[row][i] * arr2[i][col];
+                }
+            }
+        }
+        System.out.println("Result of singlethreading:");
+        System.out.println(Arrays.deepToString(singleThreadResult));
+
+        // Check if the result is correct
+        System.out.println("Result of singlethreading is equal to result of multithreading: "
+                + String.valueOf(Arrays.deepEquals(ans, singleThreadResult)));
+
     }
 }
